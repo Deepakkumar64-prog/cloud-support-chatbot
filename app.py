9ee671d4421ae15cf5cb8e18f9aa40e3adbf55b6
Deepakkumar64-prog <Deepak.kumar@rapyder.com>
2026-06-25 01:31:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ if question:
                     })
 
                 response = client.models.generate_content(
-                    model="gemini-1.5-flash",
+                    model="models/gemini-1.5-flash",
                     contents=conversation
                 )
                 answer = response.text
